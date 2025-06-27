40f68c74b117cc63523c4d14917001eadcc2171e
Ujjwal Kandel <ujjwalkandel1@gmai.com>
2025-06-27 13:57:53 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ScrollInterface from "./assets/components/ScrollInterface"
+
+
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <ScrollInterface />
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/ScrollInterface.tsx
@@ -0,0 +1,7 @@
+
+
+export const ScrollInterface = () => {
+  return (
+    <div>ScrollInterface</div>
+  )
+}
